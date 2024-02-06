4b835588bf784103c6aa906608400ac69fa0d9a2
bibin k b <bibinkochumalayil007@gmail.com>
2024-02-06 20:10:21 +0530

    Slpash screen completed

---
+++ lib/application/feature/splash/splash_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage('assets/images/Lottie.gif'))),
+      ),
+    );
+  }
+}
